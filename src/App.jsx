1c4a286d31517bb28aef583b8a50a50233c19bd0
Ra <132078939+RPRd001@users.noreply.github.com>
2024-05-25 10:08:55 -0400

    Components files created & tailwind installed & configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Navigation from './components/Navigation'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Navigation />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+        <Hero />
+        {/* Right side features */}
+        <Features />
+        <Ribbon />
+        {/* Left side features */}
+        <Features />
+    </main>
+  )
+}
+
+export default Main
